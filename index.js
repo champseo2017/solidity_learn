08bb53bf6a38f81b8e49b99185525447474ac1d9
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-03-03 21:02:22 +0700

    ทำงานกับ Structs และ Arrays

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 /* 
-Function Declarations การประกาศค่าฟังก์ชั่น
+ทำงานกับ Structs และ Arrays
 
-การประกาศฟังก์ชั่นใน Solidity จะมีหน้าตาดังนี้
-
-function eatHamburgers(string _name, uint _amount) {
-
+struct Person {
+  uint age;
+  string name;
 }
 
-โดยตัวอย่างดังกล่าวจะเป็นฟังก์ชั่นที่ชื่อว่า eatHamburgers ที่จะรับตัวแปร 2 ค่า ได้แก่ ข้อมูลชนิด string และ uint โดยในตอนนี้ภายในฟังก์ชั่นจะยังไม่มีอะไร
+Person[] public people;
 
-โน้ต: การเริ่มต้นชื่อของตัวแปรต่างๆ ภายในฟังก์ชั่นด้วยเครื่องหมายสัญประกาศ (_)
-ใช้เพื่อที่จะแยกความแตกต่างตัวแปรเหล่านี้ออกจากตัวแป global
+ตอนนี้เราก็จะมาศึกษาว่าจะสามารถสร้าง Person ขึ้นมาใหม่ได้อย่างไร และจะเพิ่มลงไปใน array ที่ชื่อว่า people ได้อย่างไร
 
-คุณสามารถเรียกฟังก์ชั่นได้ดังนี้
-eatHamburgers("vitalik", 100);
+// สร้างPersonขึ้นมาใหม่:
+Person satoshi = Person(172, "Satoshi");
+
+// เพิ่ม element Person ที่เราสร้างขึ้นนั้นลงไปใน Array:
+people.push(satoshi); หรือ people.push(Person(16, "Vitalik"));
+
+สังเกตได้ว่า array.push() จะเป็นคำสั่งที่เพิ่มข้อมูลลงในส่วน ท้าย ของ array ดังนั้นก็จะทำให้ข้อมูลที่อยู่ใน array นั้นก็จะถูกเรียงตามลำดับที่เราเพิ่มเข้าไป ดูได้จากตัวอย่างดังต่อไปนี้
+
+uint[] numbers;
+numbers.push(5);
+numbers.push(10);
+numbers.push(15);
+// ซึ่งจะทำให้ numbersในตอนนี้มีค่า [5, 10, 15]
+
+จากนั้นก็ให้เพิ่มลงไปยัง array ที่ชื่อว่า zombies
+
+pragma solidity ^0.4.19;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function createZombie(string _name, uint _dna) {
+        // เริ่มที่ตรงนี้
+        zombies.push(Zombie(_name, _dna));
+    }
+
+}
 
 */
